f8e720367d5da041e30dd56978d5ad4c116e3831
RalphDante <ralph.comandante1@gmail.com>
2025-12-15 04:56:45 +0800

    added mobs

---
+++ src/entity/moving/Golem.java
@@ -0,0 +1,41 @@
+package entity.moving;
+
+import gfx.Animate;
+import gfx.SpriteLibrary;
+
+public class Golem extends MovingEntity{
+    public Golem(double x, double y, SpriteLibrary sprites) {
+        super(x, y, 1, sprites);
+
+        // Configure Skele stats
+        this.maxHp = 50;
+        this.currentHp = 50;
+
+        // Configure hurtbox (adjust these values to fit your skeleton sprite)
+        setHurtBox(8, 8, 48, 56); // offsetX, offsetY, width, height
+
+        animations.put("golemAnim", new Animate(sprites.get("golemAnim"), 8));
+        currentAnimation = animations.get("golemAnim");
+    }
+
+    @Override
+    public void update(){
+        // Don't move or update if dead
+        if (isDead) {
+            super.update(); // Still update animations/timers
+            return;
+        }
+
+        getMotion().setVelocityX(-0.5); // Move left slowly
+        getMotion().setVelocityY(0);
+        super.update();
+        applyMotion();
+    }
+
+    @Override
+    protected void die() {
+        super.die();
+        // Add death effects here later (death animation, drop items, etc.)
+        System.out.println("Skeleton defeated!");
+    }
+}
